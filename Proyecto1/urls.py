ef9bda848b607e7694638aa1c241a0a3641c1ece
Osvaldo Vasquez <osvaldo.vasquez@outlook.com>
2023-09-30 18:55:45 -0600

    primer parte de la APP

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import probando_template, usando_Loader, curso
+from django.urls import path, include
 
 urlpatterns = [
-    path('probando_template/', probando_template),
-    path('usando_loader/', usando_Loader),
-    path('curso/<nombre>/<numero>/', curso),
+    path('admin/', admin.site.urls),
+    path('proyecto1App/', include('proyecto1App.urls'))
 ]
--- proyecto1App/views.py
+++ proyecto1App/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def inicio(request):
+    return HttpResponse("Vista inicio")
+
+def cursos(request):
+    return HttpResponse("Vista cursos")
+
+def profesores(request):
+    return HttpResponse("Vista profesores")
+
+def estudiantes(request):
+    return HttpResponse("Vista estudiantes")
+
+def entregables(request):
+    return HttpResponse("Vista entregables")
