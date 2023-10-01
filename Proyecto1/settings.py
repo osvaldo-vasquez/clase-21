8ea1005af1c984d6cc4d675bf0a4e7ff5575e34e
Osvaldo Vasquez <osvaldo.vasquez@outlook.com>
2023-09-30 20:33:36 -0600

    App con templates sencillo

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Proyecto1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./Proyecto1/templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- proyecto1App/views.py
+++ proyecto1App/views.py
@@ -1,16 +1,17 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    return HttpResponse("Vista inicio")
+    return render(request, "proyecto1App/index.html")
 
 def cursos(request):
-    return HttpResponse("Vista cursos")
+    return render(request, "proyecto1App/cursos.html")
 
 def profesores(request):
-    return HttpResponse("Vista profesores")
+    return render(request, "proyecto1App/profesores.html")
 
 def estudiantes(request):
-    return HttpResponse("Vista estudiantes")
+    return render(request, "proyecto1App/estudiantes.html")
 
 def entregables(request):
-    return HttpResponse("Vista entregables")
+    return render(request, "proyecto1App/entregables.html")
