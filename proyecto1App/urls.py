25ded376d1f27e923e2c92c5e60bf5498325052b
Osvaldo Vasquez <osvaldo.vasquez@outlook.com>
2023-10-05 17:44:53 -0600

    Con navegación entre ventanas

--- proyecto1App/urls.py
+++ proyecto1App/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from proyecto1App import views
 
 urlpatterns = [
-    path('index/', views.inicio),
-    path('cursos/', views.cursos),
-    path('profesores/', views.profesores),
-    path('estudiantes/', views.estudiantes),
-    path('entregables/', views.entregables)
+    path('index/', views.inicio, name = "Inicio"),
+    path('cursos/', views.cursos, name ="Cursos"),
+    path('profesores/', views.profesores, name = "Profesores"),
+    path('estudiantes/', views.estudiantes, name = "Estudiantes"),
+    path('entregables/', views.entregables, name = "Entregables")
 ]
